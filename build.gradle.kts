c2ae434d93115b8a1ee6e35ff985b3687545c1de
paulmiddelkoop <paul@pamisoft.nl>
2022-02-08 15:51:59 +0100

    Added code coverage to build.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.api.JavaVersion.VERSION_17
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.run.BootRun
 
@@ -7,12 +8,13 @@ plugins {
 	id("org.flywaydb.flyway") version "8.4.4"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
+	jacoco
 }
 apply(plugin = "io.spring.dependency-management")
 
 group = "se.pamisoft"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = VERSION_17
 
 repositories {
 	mavenCentral()
@@ -32,6 +34,8 @@ dependencies {
 	implementation("dev.personnummer:personnummer:3.3.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.mockito.kotlin:mockito-kotlin:${dependencyManagement.importedProperties["mockito.version"]}")
+	jacocoAnt("org.jacoco:org.jacoco.ant")
+	jacocoAgent("org.jacoco:org.jacoco.agent")
 }
 
 tasks.withType<KotlinCompile> {
@@ -47,6 +51,13 @@ tasks.getByName<BootRun>("bootRun") {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+	reports {
+		xml.required.set(true)
+		html.required.set(false)
+	}
 }
 
 flyway {
