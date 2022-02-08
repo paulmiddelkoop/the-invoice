a9256d3ba8bdeedab563041e59aa5884f22ae7e6
paulmiddelkoop <paul@pamisoft.nl>
2022-02-08 16:51:51 +0100

    Added assert

--- src/test/kotlin/se/pamisoft/theinvoice/ApplicationIT.kt
+++ src/test/kotlin/se/pamisoft/theinvoice/ApplicationIT.kt
@@ -1,13 +1,16 @@
 package se.pamisoft.theinvoice
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ApplicationIT {
+class ApplicationIT(@Autowired private val familyController: FamilyController) {
 	@Test
 	fun `Should load context`() {
+		assertThat(familyController).isNotNull
 	}
 }
